514818ae8c8e799ab0d7b8ab7a6e7862df9cc93a
guanabara-eteot <91096819+guanabara-eteot@users.noreply.github.com>
2021-10-04 20:46:17 -0300

    exercício 009

---
+++ javascript/ex009/mercado.js
@@ -0,0 +1,15 @@
+// Esta área executará sempre que a página for carregada
+document.getElementById("resultado").style.visibility = "hidden"
+
+// Esta função só executará quando clicarmos no botão submit
+function reajuste() {
+    let nome = document.getElementById("produto").value
+    let pre = Number(document.getElementById("preco").value)
+    let dezporc = (pre * 10)/100
+    let novopre = pre - dezporc
+
+    let saida = document.getElementById("saida")
+    saida.innerHTML = `O produto <strong>${nome}</strong> vai custar R$${novopre} na promoção de aniversário! `
+    document.getElementById("resultado").style.visibility = "visible"
+    return false
+}
